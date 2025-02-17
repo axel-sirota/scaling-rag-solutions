2c70b7e5abcba24b1b15992f056d588cb87c4f0e
Axel Sirota <axel.sirota@gmail.com>
2025-02-17 10:06:51 -0300

    final

--- infrastructure/modules/ecs/ecs-task.tf
+++ infrastructure/modules/ecs/ecs-task.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "rag_task" {
   family                   = "rag-task"
   requires_compatibilities = ["EC2"]
-  memory                   = "32768"
+  memory                   = "8192"
   cpu                      = "4096"
   network_mode             = "bridge"
   
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "rag_task" {
       name         = "rag-container"
       image        = "253957294717.dkr.ecr.us-east-1.amazonaws.com/scaled-rag-repo:latest"
       essential    = true
-      memory       = 32768
+      memory       = 8192
       cpu          = 4096
       resourceRequirements = [
         {
@@ -18,12 +18,12 @@ resource "aws_ecs_task_definition" "rag_task" {
           value = "1"
         }
       ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options   = {
-          "awslogs-group"         = "/ecs/rag-service"
-          "awslogs-region"        = "us-east-1"
-          "awslogs-stream-prefix" = "rag-container"
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options"   : {
+          "awslogs-group"         : "/ecs/rag-service",
+          "awslogs-region"        : "us-east-1",
+          "awslogs-stream-prefix" : "rag-container"
         }
       }
       portMappings = [
